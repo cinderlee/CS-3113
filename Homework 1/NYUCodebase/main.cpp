893bfd04c42f780351746c2b25e7d683e546061f
cindylee0113 <cindylee0113@gmail.com>
2018-02-11 16:14:22 -0500

    Included untextured code

--- Homework 1/NYUCodebase/main.cpp
+++ Homework 1/NYUCodebase/main.cpp
@@ -15,6 +15,8 @@
     #define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
 #endif
 
+using namespace std;
+
 SDL_Window* displayWindow;
 
 GLuint LoadTexture(const char *filePath) {
@@ -37,7 +39,7 @@ GLuint LoadTexture(const char *filePath) {
 
 int main(int argc, char *argv[])
 {
-   
+    
     SDL_Init(SDL_INIT_VIDEO);
     displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 880, 450, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
@@ -54,33 +56,45 @@ int main(int argc, char *argv[])
     ShaderProgram program;
     program.Load(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
     GLuint toukenTexture = LoadTexture(RESOURCE_FOLDER"Mikazuki-2.png");
-    GLuint toukenotherTexture = LoadTexture(RESOURCE_FOLDER"touken2.gif");
+    GLuint articuno = LoadTexture(RESOURCE_FOLDER"Articuno.png");
     GLuint sakura = LoadTexture(RESOURCE_FOLDER"sakura.png");
     GLuint background = LoadTexture (RESOURCE_FOLDER"Kanda_Shrine.png");
     
     ShaderProgram programUntextured;
+    programUntextured.Load(RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragment.glsl");
     
     float lastFrameTicks = 0.0f;
     float angle = 0.0f;
     float back = -1.0f;
     float totalTime = 0.0f;
-    
+    float move = 0.0f;
+    float direction = -1.0f;
+    float current_position = 1.5f;
     
     Matrix projectionMatrix;
     Matrix modelMatrix;
-    modelMatrix.Translate(-1.2f, -1.0f, 0.0f);
-    modelMatrix.Rotate (0.3);
-//    modelMatrix.Scale(1.5f, 1.5f, 1.0f);
+    modelMatrix.Translate(-1.2f, -1.3f, 0.0f);
+    modelMatrix.Rotate (0.25);
+    modelMatrix.Scale(0.75f, 0.75f, 1.0f);
     Matrix viewMatrix;
     projectionMatrix.SetOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
    
     Matrix modelMatrix2;
-    modelMatrix2.Translate(-1.75f, -0.25f, 0.0f);
+    modelMatrix2.Translate(-1.75f, -0.55f, 0.0f);
     modelMatrix2.Scale (3.5f, 3.5f, 1.0f);
     
+    Matrix modelMatrix3;
+    modelMatrix3.Translate(2.0f, 1.5f, 0.0f);
+    
     Matrix modelBackground;
-    program.SetModelMatrix (modelBackground);
     modelBackground.Scale (1.80f, 1.5f, 1.0f);
+    
+    Matrix shapes;
+    Matrix viewMatrixUn;
+    shapes.Translate (2.0f, 1.0f, 0.0f);
+    
+    glUseProgram(program.programID);
+    glUseProgram(programUntextured.programID);
     
     SDL_Event event;
     bool done = false;
@@ -106,8 +120,6 @@ int main(int argc, char *argv[])
         lastFrameTicks = ticks;
         
         
-        glUseProgram(program.programID);
-        
         program.SetModelMatrix(modelBackground);
         glBindTexture(GL_TEXTURE_2D, background);
         float vertices3[] = {-2, -1.5, 2, -1.5, 2, 1.5, -2, -1.5, 2, 1.5, -2, 1.5};
@@ -128,13 +140,30 @@ int main(int argc, char *argv[])
         glEnableVertexAttribArray(program.texCoordAttribute);
         glDrawArrays(GL_TRIANGLES, 0, 6);
         
+        program.SetModelMatrix(modelMatrix3);
+        glBindTexture(GL_TEXTURE_2D, articuno);
+        current_position += elapsed * direction;
+        move = elapsed;
+        modelMatrix3.Translate(elapsed * direction, 0.0f, 0.0f);
+        if (current_position <= -3.55 || current_position + 1 >= 3.55){
+            direction *= -1;
+        }
+        
+        float vertices4[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices4);
+        glEnableVertexAttribArray(program.positionAttribute);
+        float texCoords4[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords4);
+        glEnableVertexAttribArray(program.texCoordAttribute);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
         program.SetModelMatrix(modelMatrix);
         glBindTexture(GL_TEXTURE_2D,toukenTexture);
-        totalTime += elapsed;
-        angle = elapsed * back  ;
+        totalTime += 0.4 * elapsed;
+        angle = elapsed * back ;
         modelMatrix.Rotate (angle);
         
-        if (totalTime > 0.3) {
+        if (totalTime > 0.2) {
             back *= -1;
             totalTime = 0;
         }
@@ -150,11 +179,20 @@ int main(int argc, char *argv[])
         glEnableVertexAttribArray(program.texCoordAttribute);
         glDrawArrays(GL_TRIANGLES, 0, 6);
         
-
-        
         glDisableVertexAttribArray(program.positionAttribute);
         glDisableVertexAttribArray(program.texCoordAttribute);
     
+        
+        programUntextured.SetModelMatrix(shapes);
+        programUntextured.SetViewMatrix(viewMatrixUn);
+        programUntextured.SetProjectionMatrix(projectionMatrix);
+        programUntextured.SetColor(0.5, 0, 0.9, 0.5f);
+        glUseProgram(programUntextured.programID);
+        float verticesUntextured[] = {0.85f, 0.50f, 0.80f, 0.75f, 0.75f, 0.5f, 0.85f, 0.50f, 0.75f, 0.5f, 0.80, 0.25, 0.80, 0.55, 0.80, 0.45, 1.05, 0.5, 0.80, 0.55, 0.80, 0.45, 0.55, 0.5};
+        glVertexAttribPointer(programUntextured.positionAttribute, 2, GL_FLOAT, false, 0, verticesUntextured);
+        glEnableVertexAttribArray(programUntextured.positionAttribute);
+        glDrawArrays(GL_TRIANGLES, 0, 12);
+        glDisableVertexAttribArray(programUntextured.positionAttribute);
         
         SDL_GL_SwapWindow(displayWindow);
     }
